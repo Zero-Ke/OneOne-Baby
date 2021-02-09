0860c1b001f2bc4c2a191cf14b2ec86aada00ea7
谭华泽 <tanhuaze@ZeroKe-2.local>
2021-02-09 20:45:47 +0800

    commit

--- ReadMe.md
+++ ReadMe.md
@@ -2,3 +2,6 @@
     npm config set registry https://registry.npm.taobao.org
 ## 2.查看结果
     npm config get registry
+## 3.创建项目
+    1.npm install -g create-react-app
+    2.create-react-app app
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app'
+// const content = "Hello World";
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>{content}</h1>
+//       <h2>Time = {new Date().toLocaleTimeString()}</h2>
+//     </div>
+//   )
+//   ReactDOM.render(element, document.getElementById('root'));
+// };
+// setInterval(tick, 1000);
+
+ReactDOM.render(<App />, document.getElementById('root'))
